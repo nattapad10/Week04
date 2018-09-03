52e719b88cc811c761e92171e059b68a9fb28ef9
aun <nattapad10@gmail.com>
2018-09-03 22:20:21 +0700

    W04P03

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -6,8 +6,8 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("world");
         }
     }
 }

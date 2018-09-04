8805ff4b66feffee6963cec8f7f845405794de07
aun <nattapad10@gmail.com>
2018-09-04 18:50:22 +0700

    W04P11

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -7,11 +7,11 @@ namespace Lab04
         static void Main(string[] args)
         {
             string str;
-            Console.Write("Enter some characters. ");
-            str = Console.Read();
-            Console.WriteLine("You entered: " + str);
+            Console.Write("Please enter value 1 : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
         }
-    }
+    
     
     }
 }

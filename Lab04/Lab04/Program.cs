1a451cf2126481f8f43d76f2f0e2a6d190246095
aun <nattapad10@gmail.com>
2018-09-08 12:08:55 +0700

    W04P17

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -6,14 +6,82 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
-            int a, b, c, d, e, f;
-            a = 1;
-            b = a + 6;
-            c = b - 3;
-            d = c * 2;
-            e = d / 2;
-            f = e % 2;
-            
+            int a = 10, b = 20, x = 5, y = 2;
+            Double a1 = 0;
+            a1= a + b;
+            Console.WriteLine("..a + b....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = x - b;
+            Console.WriteLine("..x-b....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = x * b;
+            Console.WriteLine("..x*b....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = y / a;
+            Console.WriteLine("..y/a....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = b % y;
+            Console.WriteLine("..b%y....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = y + 10 % x;
+            Console.WriteLine("..y+10%x....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = a / 3 * 5;
+            Console.WriteLine("..a/3*5....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = 9 / 2 * a;
+            Console.WriteLine("..9/2*a....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = y % 8;
+            Console.WriteLine("..y%8....{0}", a1);
+
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            a1 = 0;
+            a1 = 100 * x + y % 2 - a;
+            Console.WriteLine("..100*x+y%2-a....{0}", a1);
             Console.ReadKey();
 
             
